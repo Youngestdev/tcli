ded6c79ea89de9a5164da3beab0803d3b89f6e1c
Abdulazeez Abdulazeez Adeshina <laisibizness@gmail.com>
2019-01-26 04:02:39 +0100

    Moved client to lib

--- client.js
+++
@@ -1,9 +0,0 @@
-const Twitter = require("twitter");
-
-const config = {
-  consumer_key: YOUR_CONSUMER_KEY,
-  consumer_secret: YOUR_CONSUMER_SECRET,
-  bearer_token: YOUR_BEARER_TOKEN
-};
-
-module.exports = new Twitter(config);
